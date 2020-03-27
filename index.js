83f4562775f63f1fbe31b227c0f8bccb61c77df2
LKNorman <lknorman30@gmail.com>
2020-03-26 22:39:59 -0400

    added write function

--- index.js
+++ index.js
@@ -23,4 +23,25 @@ function promptUser() {
         name: "license"
       }
     ])
-  }
+    // function to write the actual readme file as well as pulling in a default looking badge and the table of contents
+    .then(function(answer) {
+      fs.writeFile(
+        "READMEGeneratorTest.md",
+        `# Title: ${answer.title}\n\n![Simple Badge](https://img.shields.io/badge/License-${answer.license}-<COLOR>.svg\n\n## Description: ${answer.description}\n\n## Table of Constents
+          1. How to Install
+
+          2. How to use this program
+
+          3. Other contributors on the project
+
+          4. Tests
+
+          5. Github Information: \n\n`,
+        function(err) {
+          if (err) {
+            console.log(err);
+          }
+        }
+      );
+    })
+}
